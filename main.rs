847b447fac9d78efaaea9335417a8c4e0c6cdd37
John Bannick <jbannick@7128.com>
2024-08-01 17:31:35 -0400

    Added DDS setup. Not loop.

--- main.rs
+++ main.rs
@@ -1,12 +1,19 @@
 use std::{io, time};
 use std::env;
 
+use rustdds::*;
+use rustdds::no_key::{DataReader, DataWriter, DataSample}; // We use a NO_KEY topic here
+use serde::{Serialize, Deserialize};
+
 use log4rs::{
   append::console::ConsoleAppender,
   config::{Appender, Root},
   Config,
 };
 use log::LevelFilter;
+use futures::StreamExt;
+use smol::Timer;
+
 use log::{debug, error, info, trace, warn};
 
 #[derive(Debug)]
@@ -31,10 +38,10 @@ fn main() {
       let argval = &args[1];
       match argval.as_str() {
         "Sub" => {
-          println!("Server is Subscriber");
+          // println!("Server is Subscriber");
         },
         "Pub" => {
-        println!("Server is Publisher");
+          // println!("Server is Publisher");
         },
         _ => {
         println!("***ERROR: Invalid argument {:?}", argval);
@@ -52,17 +59,72 @@ fn main() {
   configure_logging();
   
   println!("Structure: {:?}", Structure(3));
-  
   info!("Deep: {:?}", Deep(Structure(0)));
   debug!("Deep: {:?}", Deep(Structure(7)));
   trace!("Deep: {:?}", Deep(Structure(77)));
   warn!("Deep: {:?}", Deep(Structure(999)));    
   error!("Deep: {:?}", Deep(Structure(666)));
+
+  // Create a DDS DomainParticipant
+  let domain_participant = DomainParticipant::new(0).unwrap();
+  info!("Created the DomainParticipant");
+  
+  // Create a DDS Quality of Service
+  let qos = QosPolicyBuilder::new()
+    .reliability(policy::Reliability::Reliable {max_blocking_time: rustdds::Duration::ZERO})
+    .build();
+  info!("Created the Quality of Service");
+  
+  // Create a DDS Subscriber 
+  let subscriber = domain_participant.create_subscriber(&qos).unwrap();
+  info!("Created the DDS Subscriber");
+  
+  // Create a DDS Publisher
+  let publisher = domain_participant.create_publisher(&qos).unwrap();
+  info!("Created the DDS Publisher");
+  
+  // Create the DDS Topic
+  let some_topic = domain_participant.create_topic("some_topic".to_string(), "SomeType".to_string(), &qos, TopicKind::NoKey).unwrap();
+  info!("Created the DDS Topic");
+  
+  #[derive(Serialize, Deserialize, Debug)]
+  struct SomeType {
+    a: i32
+  }
+  
+  let mut reader = subscriber
+    .create_datareader_no_key::<SomeType, CDRDeserializerAdapter<SomeType>>(
+      &some_topic,
+      None)
+    .unwrap();
+  info!("Created the DDS DataReader");
+  
+  let mut writer = publisher
+    .create_datawriter_no_key::<SomeType, CDRSerializerAdapter<SomeType>> (
+      &some_topic,
+      None)
+    .unwrap();
+  info!("Created the DDS DataWriter");
+  
+  // ---
+  
+  let pub_or_sub = "Sub";
+  
+  info!("pub_or_sub = {:?}", pub_or_sub);
+  if "Sub" == pub_or_sub {
+    println!("Server is Subscriber");
+  } else if "Pub" == pub_or_sub {
+    println!("Server is Publisher");
+  } else {
+    error!("Invalid argument: {:?}", pub_or_sub);
+    return;
+  }
+  
 }
 
 
 fn configure_logging() {
-  debug!("Enter: configure_logging");
+  println!("Enter: configure_logging");
   
   log4rs::init_file(
     "logging-config.yaml",
